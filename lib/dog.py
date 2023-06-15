a5b1cee4a026404aa27bb9ecdfd96a4c07a1ee8a
Mai <garcia.ton94@gmail.com>
2023-06-15 15:51:15 -0400

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -8,8 +8,42 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
-    "Pointer"
+    "Pointer",
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self.name = name
+
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, breed):
+        self._breed = breed
+
+    def __str__(self):
+        return f"I am a {self._breed}. My name is {self._name}"
+
+
+fido = Dog("Fido", "Beagle")
+
+print(fido)
--- lib/person.py
+++ lib/person.py
@@ -12,8 +12,42 @@ APPROVED_JOBS = [
     "General Management",
     "Research & Development",
     "Marketing",
-    "Purchasing"
+    "Purchasing",
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="Mai", job="Legal"):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        self._job = job
+
+    def __str__(self):
+        return f"My name is {self._name}. I work in {self._job}"
+
+
+mai = Person("Mai", "Human Resources")
+
+print(mai)
